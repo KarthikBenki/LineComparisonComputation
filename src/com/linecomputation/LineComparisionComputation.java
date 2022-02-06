bb875cdd1a63d493810aba08dbc1b030ed4e51dc
karthikbenki <karthikmc007@gmail.com>
2022-02-06 14:21:55 +0530

    Added: Calculating the length using cartesian logic

--- src/com/linecomputation/LineComparisionComputation.java
+++ src/com/linecomputation/LineComparisionComputation.java
@@ -5,7 +5,7 @@ public class LineComparisionComputation {
 	public static int lineLengthCal() {
 		int x1,y1,x2,y2;
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter the co-ordinates of line: x1,x2,x3,x4"); 
+		System.out.println("Enter the co-ordinates of line!!: x1,x2,x3,x4"); 
 		x1 = scanner.nextInt();
 		y1 = scanner.nextInt();
 		x2 = scanner.nextInt();
